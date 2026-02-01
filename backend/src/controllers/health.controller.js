f2097a4d1a2939d2cbb4cd3a7a4344890bdbf4fd
sh1vam <balaji030204@gmail.com>
2026-02-02 02:03:34 +0530

    feat: Add ADMIN role requirement to the health check endpoint.

--- backend/src/controllers/health.controller.js
+++ backend/src/controllers/health.controller.js
@@ -1,9 +1,10 @@
+import asyncHandler from "../utils/asyncHandler.js";
+
 /**
  * @route GET /api/health
  * @desc Health check
  * @access Public
  */
-import asyncHandler from "../utils/asyncHandler.js";
 
 const healthCheck = asyncHandler(async (req, res) => {
     res.json({ message: "Api is working..." });
