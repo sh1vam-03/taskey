237c4449a2d5c3a6a4327f24bd002152acce38d2
sh1vam <balaji030204@gmail.com>
2026-02-19 03:40:54 +0530

    chore: Remove duplicate "SECTIONS" comments.

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -95,8 +95,6 @@ export default function Home() {
 
             {/* SECTIONS */}
 
-            {/* SECTIONS */}
-
             <BentoGridSection />
             <DetailedFeatures />
             <InteractivePricing />
